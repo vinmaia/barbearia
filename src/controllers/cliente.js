68083d2f00fa31c9fba442358a285313bf0a1cb5
Maia <vincecake008@gmail.com>
2024-08-28 11:41:01 -0300

    feat: new debug

--- src/controllers/cliente.js
+++ src/controllers/cliente.js
@@ -4,86 +4,48 @@
  - email
  - senha?*/
 
-const db = [];
-let nextId = 1;
+ const model = require("../models/cliente.js")
 
-const model = (body, id = nextId++) => {
-  const telefone = body.telefone
-    .replaceAll("-", "")
-    .replaceAll("(", "")
-    .replaceAll(")", "")
-    .replaceAll(" ", "");
-
-    const soNumero = true
-
-    telefone.forEach(el => {
-        if(isNaN(Number(el))) {
-            soNumero = false
-        } 
-    })
-  if (
-    body.nome != undefined &&
-    body.nome != "" &&
-    body.telefone != undefined &&
-    telefone != "" &&
-    body.email != undefined &&
-    body.email != "" &&
-    body.senha != undefined &&
-    body.senha != "" &&
-    telefone.length >= 11 &&
-    body.email.includes("@") &&
-    telefone.length <= 12
-  ) {
-    return {
-      id,
-      nome: body.nome,
-      telefone: body.telefone,
-      email: body.email,
-      senha: body.senha,
-    };
-  }
-};
-
-const store = (body) => {
-  const novo = model(body);
-
-  if (novo) {
-    db.push(novo);
-    return 201;
-  }
-
-  return 400;
-};
-
-const index = () => db;
-
-const show = (id) => db.find((el) => el.id == id);
-
-const update = (id, body) => {
-  const index = db.findIndex((el) => el.id == id);
-  const novo = model(body, parseInt(id));
-
-  if (novo && index != -1) {
-    db[index] = novo;
-
-    return 200;
-  }
-
-  return 400;
-};
-
-const destroy = (id) => {
-  const index = db.findIndex((el) => el.id == id);
-
-  if (index != -1) {
-    db.splice(index, 1);
-  }
-};
-
-module.exports = {
-  store,
-  index,
-  show,
-  update,
-  destroy,
-};
+ const db = []
+ 
+ const index = () => db
+ 
+ const show = id => db.find(el => el.id == id)
+ 
+ const store = (body) => {
+     const novo = model(body)
+ 
+     if (novo) {
+         db.push(novo)
+         return 201
+     }
+ 
+     return 400
+ }
+ 
+ const update = (body, id) => {
+     const novo = model(body, parseInt(id))
+     const indice = db.findIndex(el => el.id == id)
+ 
+     if (novo && indice != -1) {
+         db[indice] = novo
+         return 200
+     }
+ 
+     return 400
+ }
+ 
+ const destroy = id => {
+     const indice = db.findIndex(el => el.id == id)
+     if (indice != -1) {
+         db.splice(indice, 1)
+     }
+ }
+ 
+ module.exports = {
+     index,
+     show,
+     store,
+     update,
+     destroy
+ }
